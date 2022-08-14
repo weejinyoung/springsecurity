b80276f62cda3a7e2a6b09d720a7aa6c9ae897f4
Jn W <a8118199@gmail.com>
2022-08-14 23:41:10 +0900

    엔티티 생성과 데이터베이스 연결, security기본 설정

--- src/main/java/com/study/springsecurity/controller/HelloController.java
+++ src/main/java/com/study/springsecurity/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.study.springsecurity.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ src/main/java/com/study/springsecurity/domain/Authority.java
@@ -0,0 +1,19 @@
+package com.study.springsecurity.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "authority")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Authority {
+
+    @Id
+    @Column(name = "authority_name", length = 50)
+    private String authorityName;
+}
